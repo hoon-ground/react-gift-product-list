068ecde745c7e0853ca6a7547bf7d303b4134bc6
hoon_ground <break_the_limit@naver.com>
2025-07-17 20:02:41 +0900

    feat : authToken 자동 헤더 부착

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { getUserInfo } from '@/utils/storage';
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -6,6 +7,14 @@ const axiosInstance = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const user = getUserInfo();
+  if (user?.authToken) {
+    config.headers.Authorization = user.authToken;
+  }
+  return config;
 });
 
 export default axiosInstance;
